ed52aca95f58b26932aca287b5b4fa3938391d61
zhekaasd <zhekaasd13@gmail.com>
2020-09-11 08:39:02 +0400

    Updates

---
+++ precache-manifest.e07471551c1c72d406db19ff9c7ec3d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a489f61740960ed17d53af03bd5d183b",
+    "url": "/counter-task/index.html"
+  },
+  {
+    "revision": "4abd1ea161d5be295591",
+    "url": "/counter-task/static/css/main.2e238f31.chunk.css"
+  },
+  {
+    "revision": "bf3348cf0cdb33f9be59",
+    "url": "/counter-task/static/js/2.5d2f6e92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter-task/static/js/2.5d2f6e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4abd1ea161d5be295591",
+    "url": "/counter-task/static/js/main.c5597ec2.chunk.js"
+  },
+  {
+    "revision": "c6a1ee98716ef3917252",
+    "url": "/counter-task/static/js/runtime-main.43d5e0ca.js"
+  }
+]);
